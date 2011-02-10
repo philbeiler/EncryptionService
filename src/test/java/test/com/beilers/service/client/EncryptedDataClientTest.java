d0e334a06443d65b3ef95f6071c1394695ca1f57
Phil Beiler <phil@beilers.com>
2011-02-10 09:31:30 -0500

    pom details added

--- src/test/java/test/com/beilers/service/client/EncryptedDataClientTest.java
+++ src/test/java/test/com/beilers/service/client/EncryptedDataClientTest.java
@@ -10,7 +10,7 @@ import com.beilers.client.encryption.EncryptedDataClient;
 
 public class EncryptedDataClientTest extends UnitTestHelper {
 
-    private static final String URI                   = "http://localhost:9090/EncryptionService";
+    private static final String URI                   = "http://localhost:8080/EncryptionService";
     private static final String DATABASE_PASSWORD_KEY = "database.password";
     private static final Logger LOGGER                = LoggerFactory.getLogger(EncryptedDataClientTest.class);
 
